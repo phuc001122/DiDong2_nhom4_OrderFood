ce8e6ceea79cc84067aea5f7109252d316e6757c
ngdongduy <ngdongduy@gmail.com>
2019-05-06 02:32:38 +0700

    Create ItemClickListener.java

---
+++ client/app/src/main/java/com/chungnguyen/orderfood/Interface/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.chungnguyen.orderfood.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick(View view , int posittion, boolean isLongClick);
+}
